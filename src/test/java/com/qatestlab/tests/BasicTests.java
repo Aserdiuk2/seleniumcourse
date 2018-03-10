2b98c9b56768a5aec8dae320689477c6a4f12e56
Aserdiuk2 <aserdiuk2@gmail.com>
2018-03-10 15:26:10 +0200

    Добавлены Implicit / Explicit waits

--- src/test/java/com/qatestlab/tests/BasicTests.java
+++ src/test/java/com/qatestlab/tests/BasicTests.java
@@ -4,61 +4,63 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class BasicTests {
 
     @Test
-    public static void main (String[] args) throws InterruptedException {
+    public static void main (String[] args) {
         WebDriver driver = initChromeDriver();
 
         driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0");
-        Thread.sleep(3000);
+
         WebElement login = driver.findElement(By.id("email"));
         login.sendKeys("webinar.test@gmail.com");
 
         WebElement password = driver.findElement(By.id("passwd"));
         password.sendKeys("Xcg7299bnSmMuRLp9ITw");
-        Thread.sleep(3000);
 
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("submitLogin")));
         WebElement submitButton = driver.findElement(By.name("submitLogin"));
         submitButton.click();
-        Thread.sleep(3000);
 
+        WebDriverWait wait2 = new WebDriverWait(driver, 10);
+        wait2.until(ExpectedConditions.visibilityOfElementLocated(By.className("employee_avatar_small")));
         WebElement avatarMenu = driver.findElement(By.className("employee_avatar_small"));
         avatarMenu.click();
-        Thread.sleep(3000);
 
         WebElement logoutButton = driver.findElement(By.xpath("//*[@id=\"header_logout\"]"));
         logoutButton.click();
-        Thread.sleep(3000);
 
         driver.quit();
 
     }
 
     @Test
-    public void mainTabTests() throws InterruptedException {
+    public void mainTabTests() {
         WebDriver driver = initChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0");
-        Thread.sleep(3000);
         WebElement login = driver.findElement(By.id("email"));
         login.sendKeys("webinar.test@gmail.com");
 
         WebElement password = driver.findElement(By.id("passwd"));
         password.sendKeys("Xcg7299bnSmMuRLp9ITw");
-        Thread.sleep(3000);
 
         WebElement submitButton = driver.findElement(By.name("submitLogin"));
         submitButton.click();
-        Thread.sleep(3000);
 
         WebElement ordersSubtab = driver.findElement(By.id("subtab-AdminParentOrders"));
         ordersSubtab.click();
-        Thread.sleep(3000);
         String title = driver.getTitle();
         System.out.println(title);
         driver.navigate().refresh();
@@ -66,7 +68,6 @@ public class BasicTests {
 
         WebElement catalogSubtab = driver.findElement(By.id("subtab-AdminCatalog"));
         catalogSubtab.click();
-        Thread.sleep(3000);
         String title2 = driver.getTitle();
         System.out.println(title2);
         driver.navigate().refresh();
@@ -74,10 +75,8 @@ public class BasicTests {
 
         WebElement logoButton = driver.findElement(By.xpath("/html/body/header/nav/a"));
         logoButton.click();
-        Thread.sleep(3000);
         WebElement customersSubtab = driver.findElement(By.id("subtab-AdminParentCustomer"));
         customersSubtab.click();
-        Thread.sleep(3000);
         String title3 = driver.getTitle();
         System.out.println(title3);
         driver.navigate().refresh();
@@ -85,7 +84,6 @@ public class BasicTests {
 
         WebElement customerThreadsSubtab = driver.findElement(By.id("subtab-AdminParentCustomerThreads"));
         customerThreadsSubtab.click();
-        Thread.sleep(3000);
         String title4 = driver.getTitle();
         System.out.println(title4);
         driver.navigate().refresh();
@@ -93,7 +91,6 @@ public class BasicTests {
 
         WebElement statsSubtab = driver.findElement(By.id("subtab-AdminStats"));
         statsSubtab.click();
-        Thread.sleep(3000);
         String title5 = driver.getTitle();
         System.out.println(title5);
         driver.navigate().refresh();
@@ -101,7 +98,6 @@ public class BasicTests {
 
         WebElement adminDashboardButton = driver.findElement(By.xpath("//*[@id=\"tab-AdminDashboard\"]/a/span"));
         adminDashboardButton.click();
-        Thread.sleep(3000);
         String title6 = driver.getTitle();
         System.out.println(title6);
         driver.navigate().refresh();
