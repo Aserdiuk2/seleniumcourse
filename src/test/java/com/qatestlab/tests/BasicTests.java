55fe412d63b15dd790bed95b7a43d5a264dee44b
Aserdiuk2 <aserdiuk2@gmail.com>
2018-03-10 17:33:03 +0200

    Добавлен тест из лекции 4

--- src/test/java/com/qatestlab/tests/BasicTests.java
+++ src/test/java/com/qatestlab/tests/BasicTests.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -12,12 +14,18 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
+
 
 public class BasicTests {
 
     @Test
     public static void main (String[] args) {
         WebDriver driver = initChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        EventFiringWebDriver webDriver = new EventFiringWebDriver(driver);
+        webDriver.register(new EventHandler());
+
 
         driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0");
 
@@ -27,13 +35,59 @@ public class BasicTests {
         WebElement password = driver.findElement(By.id("passwd"));
         password.sendKeys("Xcg7299bnSmMuRLp9ITw");
 
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("submitLogin")));
+        wait.until(visibilityOfElementLocated(By.name("submitLogin")));
         WebElement submitButton = driver.findElement(By.name("submitLogin"));
         submitButton.click();
 
-        WebDriverWait wait2 = new WebDriverWait(driver, 10);
-        wait2.until(ExpectedConditions.visibilityOfElementLocated(By.className("employee_avatar_small")));
+        wait.until(visibilityOfElementLocated(By.id("subtab-AdminCatalog")));
+        WebElement orderTabElement = driver.findElement(By.id("subtab-AdminCatalog"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(orderTabElement).build().perform();
+
+        wait.until(visibilityOfElementLocated(By.id("subtab-AdminCategories")));
+        WebElement subtabCategories = driver.findElement(By.id("subtab-AdminCategories"));
+        subtabCategories.click();
+
+
+        wait.until(visibilityOfElementLocated(By.id("page-header-desc-category-new_category")));
+        WebElement addCategory = driver.findElement(By.id("page-header-desc-category-new_category"));
+        addCategory.click();
+
+        wait.until(visibilityOfElementLocated(By.id("name_1")));
+        WebElement createCategoryName = driver.findElement(By.id("name_1"));
+        createCategoryName.sendKeys("Тестовая Категория");
+        WebElement saveCategoryButton = driver.findElement(By.id("category_form_submit_btn"));
+        saveCategoryButton.click();
+
+        wait.until(visibilityOfElementLocated(By.name("categoryFilter_name")));
+        WebElement categoryNameField = driver.findElement(By.name("categoryFilter_name"));
+        categoryNameField.sendKeys("Тестовая Категория");
+        WebElement submitFilterButton = driver.findElement(By.xpath("//*[@id=\"submitFilterButtoncategory\"]"));
+        submitFilterButton.click();
+        
+        driver.quit();
+
+
+    }
+
+    @Test
+    public void succesfullSignIn() {
+        WebDriver driver = initChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0");
+
+        WebElement login = driver.findElement(By.id("email"));
+        login.sendKeys("webinar.test@gmail.com");
+
+        WebElement password = driver.findElement(By.id("passwd"));
+        password.sendKeys("Xcg7299bnSmMuRLp9ITw");
+
+        wait.until(visibilityOfElementLocated(By.name("submitLogin")));
+        WebElement submitButton = driver.findElement(By.name("submitLogin"));
+        submitButton.click();
+
+        wait.until(visibilityOfElementLocated(By.className("employee_avatar_small")));
         WebElement avatarMenu = driver.findElement(By.className("employee_avatar_small"));
         avatarMenu.click();
 
@@ -41,7 +95,6 @@ public class BasicTests {
         logoutButton.click();
 
         driver.quit();
-
     }
 
     @Test
